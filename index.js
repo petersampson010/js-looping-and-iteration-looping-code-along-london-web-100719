467fcf0074445efeb367448b26769b2cb1183456
Peter Sampson <peter.sampson010@gmail.com>
2019-11-18 14:48:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let array = []
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return array
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i)
+    }
+}
